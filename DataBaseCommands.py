866f1bd553a3ee9e300bdcdf867f9204ea04f475
fimpro <118761502+fimpro@users.noreply.github.com>
2024-11-04 22:57:34 +0100

    Adding Add_Class and !MojeKolo Command

--- DataBaseCommands.py
+++ DataBaseCommands.py
@@ -1,11 +1,14 @@
 from contextlib import nullcontext
-
+from utils import *
 from supabase import create_client, Client
+
+from utils import ClassMeet
+
 SupabaseClient = 0
 def connect_database():
     # Step 1: Set up Supabase credentials (replace with your actual Supabase URL and API key)
     SUPABASE_URL = "https://dbqakvurmeklbgyueptx.supabase.co"
-    SUPABASE_KEY = open('database_key.txt').readline()
+    SUPABASE_KEY = open('databasekey.txt').readline()
     # Step 2: Create a Supabase client
     global SupabaseClient
     SupabaseClient = create_client(SUPABASE_URL, SUPABASE_KEY)
@@ -25,6 +28,18 @@ def sync_members(members):
         response = SupabaseClient.table('messages').select('*').execute()
         print(response)
     pass
+def add_class(kolo: ClassMeet):
+    #Add_kolo to database
+    pass
+
+def get_class(time=14, role='all'):
+    #wybieramy koła z bazy danych dla roli i w czasie mniejszym niż time dni.
+    kolo1 = ClassMeet()
+    kolo2= ClassMeet()
+    kolo1.load_from_discord(type='1',date='2024-12-12',time='20:00-22:00',host="Fimpro",description="XD")
+    kolo2.load_from_discord(type='2',date='2024-12-14',time='20:33-22:00',host="Fimpro222",description="XD222")
+    return [kolo1,kolo2]
+
 def add_point(member, points):
     #in discord.member class, int number
     #Task add (points) points to given member
--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 import discord
+from aiohttp.helpers import method_must_be_empty_body
+from dateutil.rrule import weekday
 from discord.ext import commands, tasks
+
+from DataBaseCommands import get_class
+from botcommands import add_class
+from utils import *
 import DataBaseCommands as db
-
-
+import asyncio
+import re
+import botcommands
 # Initialize the bot with a command prefix (e.g., "!")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
+@bot.command(name="UstawKolo")
+async def add_class(ctx):
+    await botcommands.add_class(ctx, bot)
+
+@bot.command(name="kola")
+async def print_classes(ctx):
+    kola =  db.get_class()
+    i=0;
+    for kolo in kola:
+        i+=1
+        await ctx.send(f"{i}: Kolo z {kolo.type_str}: \n Data: {kolo.date} w godzinach: {kolo.time} \n "
+                       f"Prowadzi: {kolo.host}, {kolo.description}")
+
+@bot.command(name="MojeKola")
+async def print_custom_classes(ctx):
+    kola = db.get_class()
+    i = 0;
+    for kolo in kola:
+        if user_class_match(kolo.type,ctx.author.roles):
+            i += 1
+            await ctx.send(
+                f"{i}: Kolo z {kolo.type_str}: \n Data: {kolo.date} w godzinach: {kolo.time} \n Prowadzi: {kolo.host}, "
+                f"{kolo.description}")
 
 @bot.event
 async def on_ready(): #synchornizujemy bazę danych po restarcie
@@ -19,6 +49,7 @@ async def on_ready(): #synchornizujemy bazę danych po restarcie
     db.connect_database()
     db.sync_members(members)
     hourly_task.start()
+
 
 @tasks.loop(hours=1)
 async def hourly_task():
@@ -41,10 +72,13 @@ async def on_message(message):
     db.add_point(message.author,1)
     # Important: Process commands after custom logic
     await bot.process_commands(message)
+
 @bot.event
 async def on_reaction_add(reaction, user):
     #todo w zależności od kontentu wiadomości różne punkty
+    print(reaction.message.content)
     db.add_point(reaction.message.author,1)
+
 @bot.event
 async def on_reaction_remove(reaction, user):
     db.add_point(reaction.message.author,-1)
